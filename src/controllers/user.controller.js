9ba8ee9d5428093faaeb7cfb617a4e0efe734ee7
Luis Lopez <lopez.luisdev@gmail.com>
2025-03-18 11:39:03 -0500

    profile update agregado

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,13 @@
 import models from "../models";
-import { UserNotFound, UserInactive } from "../exceptions/users.exceptions";
-import { createTokens, verifyToken } from "../helpers/jwt.js";
+import {
+  UserNotFound,
+  UserInactive,
+  UserPasswordIncorrectSchema,
+} from "../exceptions/users.exceptions";
 
 import EmailServer from "../providers/mail.provider";
-
-import { validateEmail, validatePassword } from "../helpers/validateRequest";
+import { createTokens } from "../helpers/jwt.js";
+import { validateEmail } from "../helpers/validateRequest";
 
 class UserController {
   constructor() {
@@ -23,6 +26,43 @@ class UserController {
       if (!user.status) throw new UserInactive();
 
       return res.status(200).json(user);
+    } catch (error) {
+      return res.status(error?.code || 500).json({ message: error.message });
+    }
+  }
+
+  async updateProfile(req, res) {
+    const id = req.current_user;
+    if (!id) return res.status(401).json({ message: "Unauthorized" });
+    try {
+      const { name, last_name, celphone, dni, email, address } = req.body;
+      const { valid: validEmail, text: textEmail } = validateEmail(email);
+      const user = await this.model.findOne({ where: { id } });
+      if (!user) throw new UserNotFound();
+      if (!user.status) throw new UserInactive();
+
+      if (email && email !== user.email) {
+        if (!validEmail) throw new UserPasswordIncorrectSchema(textEmail);
+        const token = createTokens({ email });
+        const client_url = `${process.env.CLIENT_URL}/confirm-email`;
+        await EmailServer.send(
+          email,
+          "Please confirm you account",
+          `Confirm you account : <button> <a href='${client_url}?token=${token.accessToken}'>Confirm account</a> </button>`
+        );
+        await user.update({
+          name,
+          last_name,
+          celphone,
+          dni,
+          email,
+          address,
+          status: false,
+        });
+      } else {
+        await user.update({ name, last_name, celphone, dni, email, address });
+      }
+      return res.status(200).json({ message: "User updated successfully" });
     } catch (error) {
       return res.status(error?.code || 500).json({ message: error.message });
     }
--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -2,16 +2,28 @@ import { Router } from "express";
 import UserController from "../controllers/user.controller";
 import generalValidations from "../validations/general.validations";
 import { isAuthenticated } from "../middlewares/auth.middlewares";
+import Validation from "../validations/user.validations";
 class UserRouter {
   constructor() {
     this.router = Router();
   }
   init() {
-    return this.router.get("/profile", isAuthenticated, this.getProfile);
+    return this.router
+      .get("/profile", isAuthenticated, this.getProfile)
+      .patch(
+        "/profile",
+        isAuthenticated,
+        Validation.updateProfile(),
+        this.updateProfile
+      );
   }
   getProfile(req, res) {
     const controller = new UserController();
     controller.getMyProfile(req, res);
+  }
+  updateProfile(req, res) {
+    const controller = new UserController();
+    controller.updateProfile(req, res);
   }
 }
 export default new UserRouter();
