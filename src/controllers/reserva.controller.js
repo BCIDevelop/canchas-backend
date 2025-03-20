288ef21193647e5fd67c6fbbd69b8dd37c6a3a04
Luis Lopez <lopez.luisdev@gmail.com>
2025-03-20 15:25:45 -0500

    feat: Reserva endpoint creacion agregado, se modifico el endpoint getAvailableHours

--- src/controllers/reserva.controller.js
+++ src/controllers/reserva.controller.js
@@ -1,8 +1,12 @@
 import models from "../models";
 import { SportNotAllowed } from "../exceptions/deporte.exceptions";
-import { ReservaOutOfDate } from "../exceptions/reservas.exceptions";
+import {
+  ReservaOutOfDate,
+  ReservaTaken,
+} from "../exceptions/reservas.exceptions";
 import { Op } from "sequelize";
 import { CanchaDeporteNotFound } from "../exceptions/canchas.exceptions";
+import { tranformDateUTCTarget } from "../helpers/formatDate";
 import fs from "fs";
 
 class ReservaController {
@@ -10,6 +14,10 @@ class ReservaController {
     this.model = models.reservas;
     this.model_canchas = models.canchas;
     this.model_cancha_deporte = models.cancha_deporte;
+    this.utcOffset = -5;
+    this.today = new Date(
+      new Date().getTime() + this.utcOffset * 60 * 60 * 1000
+    );
     if (!ReservaController.horariosData) {
       const rawData = fs.readFileSync("horarios.json", "utf-8");
       ReservaController.horariosData = JSON.parse(rawData);
@@ -41,9 +49,22 @@ class ReservaController {
         id_cancha: cancha_id,
         id_instalacion: instalacion_id,
       }));
+      /* Consultamos a la base de datos de reservas con la condicion agregada que no puedna ser de mas de un dia de antiguedad y si es asi tendria que esatr pagado */
+      const yesterday =
+        new Date(); /* Estoy manehadno con UTC porque parece que la BD hace comparaicones en UTC */
+      yesterday.setDate(yesterday.getDate() - 1);
+
       const reservasRecords = await this.model.findAll({
         where: {
-          [Op.or]: conditions,
+          [Op.and]: [
+            { [Op.or]: conditions },
+            {
+              [Op.or]: [
+                { pagado: true },
+                { created_at: { [Op.gte]: yesterday } },
+              ],
+            },
+          ],
         },
       });
       /* Definimos las horas Disponibles  */
@@ -151,8 +172,62 @@ class ReservaController {
     }
   }
   async makeReservation(req, res) {
-    const { fecha, id_user, id_cancha, id_instalacion, hours } = req.body;
-    return res.status(200).json({ message: "Reserva creada" });
+    const { date, id_user, id_cancha, id_instalacion, hours } = req.body;
+    try {
+      const fechaReserva = new Date(date);
+      const formattedDate = `${String(fechaReserva.getUTCDate()).padStart(2, "0")}-${String(fechaReserva.getUTCMonth() + 1).padStart(2, "0")}-${fechaReserva.getUTCFullYear()}`;
+      /* Primero verificamos que el horario este disponible para esa fecha */
+      const hoursSchedule =
+        ReservaController.horariosData[id_cancha][formattedDate];
+      hours.forEach((hour) => {
+        if (!hoursSchedule[hour]) {
+          throw new ReservaTaken();
+        }
+      });
+      /* Luego verificamos que no este registrada */
+      const recordReserva = await this.model.findAll({
+        where: {
+          fecha: fechaReserva.toISOString().split("T")[0],
+          id_cancha,
+          id_instalacion,
+        },
+      });
+
+      const nowUTC5 = new Date(
+        new Date().getTime() + this.utcOffset * 60 * 60 * 1000
+      );
+      nowUTC5.setDate(nowUTC5.getDate() - 1);
+      if (recordReserva.length > 0) {
+        recordReserva
+          .filter((record) => {
+            const fechaCreacion = tranformDateUTCTarget(
+              record.created_at,
+              this.utcOffset
+            );
+            console.log(fechaCreacion);
+            return record.pagado || fechaCreacion >= nowUTC5;
+          })
+          .forEach((record) => {
+            const hoursReservadas = record.hours;
+            hours.forEach((hour) => {
+              if (hoursReservadas.includes(hour)) {
+                throw new ReservaTaken();
+              }
+            });
+          });
+      }
+      /* Creamos la reserva */
+      const resultReserva = await this.model.create({
+        fecha: fechaReserva.toISOString().split("T")[0],
+        id_user,
+        id_cancha,
+        id_instalacion,
+        hours,
+      });
+      return res.status(201).json(resultReserva);
+    } catch (error) {
+      return res.status(error?.code || 500).json({ message: error.message });
+    }
   }
 }
 export default ReservaController;
